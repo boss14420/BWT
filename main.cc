2fff1ae3f29a76bc2245526d9ceb29933cfac33e
BOSS14420 <firefoxlinux@gmail.com>
2014-10-03 13:27:37 +0700

    apply huffman seperately:   main.cc

--- main.cc
+++ main.cc
@@ -33,7 +33,7 @@ typedef std::int32_t index_type;
 static index_type min_run = 3;
 static index_type huffman_wlength = 8;
 static index_type block_size = 900000;
-static index_type capacity = block_size * (min_run+1)/ min_run + 1;
+static index_type capacity = block_size * 2; //(min_run+1)/ min_run + 1;
 static index_type alphabet_size = 256;
 
 static unsigned char *s1;
@@ -48,58 +48,95 @@ void encode(std::ostream& os, std::istream &is)
         if (reed == 0) break;
 
         // pass1: bwt -> s2 = bwt
+//        index_type pidx;
         index_type pidx = bwt::sais_bwt(s2, s1, SA, reed, alphabet_size);
         // pass2: mtf -> s2 = mtf(bwt())
         std::swap(s1, s2);
         mtf::mtf_encode(s2, s1, reed);
         // pass3: rle -> s2 = rle(mtf(bwt()))
         std::swap(s1, s2);
-        index_type rle_len = rle::rle_encode(s2, s1, reed, min_run);
+//        index_type rle_len = rle::rle_encode(s2, s1, reed, min_run);
+        index_type chr_len, rl_len;
+        std::tie(chr_len, rl_len) = rle::rle_encode2(s2, s2+reed, s1, reed, min_run);
         // pass4: huffman
         std::swap(s1, s2);
         std::istringstream iss;
-        iss.rdbuf()->pubsetbuf((char*)s1, rle_len);
+        iss.rdbuf()->pubsetbuf((char*)s1, chr_len);
         std::ostringstream oss;
-        Huffman h(iss, Huffman::Compress, huffman_wlength);
-        h.compress(oss);
-        auto const &str = oss.str();
+        Huffman h1(iss, Huffman::Compress, huffman_wlength);
+        h1.compress(oss);
+        auto const &str1 = oss.str();
         oss.seekp(0, std::ios::end);
-        index_type hlen = oss.tellp();
+        index_type h1len = oss.tellp();
 
         // output rle_len
-        os.write((char*)&rle_len, sizeof(pidx));
+        os.write((char*)&chr_len, sizeof(pidx));
+        os.write((char*)&rl_len, sizeof(pidx));
         // output pidx
         os.write((char*)&pidx, sizeof(pidx));
         // output encoded string
         //            os.write((char*)s2, rle_len);
-        os.write((char*)&hlen, sizeof(pidx));
-        os.write(str.c_str(), hlen);
+        os.write((char*)&h1len, sizeof(pidx));
+        os.write(str1.c_str(), h1len);
+//        os.write((char*)s2, chr_len);
+//        os.write((char*)(s2+reed), rl_len);
+
+        iss.clear();
+        iss.rdbuf()->pubsetbuf((char*)(s1+reed), rl_len);
+        Huffman h2(iss, Huffman::Compress, huffman_wlength);
+        oss.clear();
+        oss.str(std::string());
+        h2.compress(oss);
+        auto const str2 = std::move(oss.str());
+        oss.seekp(0, std::ios::end);
+        index_type h2len = oss.tellp();
+        os.write((char*)&h2len, sizeof(pidx));
+        os.write(str2.c_str(), h2len);
     }
 }
 
 void decode(std::ostream& os, std::istream &is)
 {
     while (!is.eof()) {
-        index_type pidx, rle_len, hlen;
-        is.read((char*)&rle_len, sizeof(pidx));
+        index_type pidx, chr_len, rl_len, h1len, h2len;
+        is.read((char*)&chr_len, sizeof(pidx));
+        is.read((char*)&rl_len, sizeof(pidx));
         is.read((char*)&pidx, sizeof(pidx));
-        is.read((char*)&hlen, sizeof(pidx));
-        is.read((char*)s1, hlen);
+
+        is.read((char*)&h1len, sizeof(pidx));
+        is.read((char*)s1, h1len);
+//        is.read((char*)s1, chr_len+rl_len);
         index_type reed = is.gcount();
         if (reed == 0) break;
 
-        // pass0: decode huffman
+        // pass0.1: decode huffman for chr
         std::istringstream iss;
         iss.rdbuf()->pubsetbuf((char*)s1, reed);
         std::ostringstream oss;
-        Huffman h(iss, Huffman::Decompress);
-        h.decompress(oss);
-        auto const &str = oss.str();
-        oss.seekp(0, std::ios::end);
-        auto hd_len = oss.tellp();
+        Huffman h1(iss, Huffman::Decompress);
+        h1.decompress(oss);
+        auto const &str1 = oss.str();
+//        oss.seekp(0, std::ios::end);
+//        auto hd1_len = oss.tellp();
+        std::copy(str1.begin(), str1.end(), s2);
+
+        // pass 0.2: decode huffman for run_length
+        is.read((char*)&h2len, sizeof(pidx));
+        is.read((char*)s1, h2len);
+        iss.clear();
+        iss.rdbuf()->pubsetbuf((char*)s1, is.gcount());
+        Huffman h2(iss, Huffman::Decompress);
+        oss.clear();
+        oss.str(std::string());
+        h2.decompress(oss);
+        auto const &str2 = oss.str();
+        std::copy(str2.begin(), str2.end(), s2+block_size);
 
         // pass1: decode rle -> s2 = mtf(bwt())
-        index_type orig_len = rle::rle_decode(s2, (unsigned char*)str.c_str(), hd_len, min_run);
+        std::swap(s1, s2);
+//        index_type orig_len = rle::rle_decode(s2, (unsigned char*)str.c_str(), hd_len, min_run);
+        index_type orig_len = rle::rle_decode2(s2, s1, chr_len, s1+block_size, rl_len, min_run);
+//        index_type orig_len = rle::rle_decode2(s2, s1, chr_len, s1+chr_len, rl_len, min_run);
         // pass2: decode mtf -> s2 = bwt
         std::swap(s1, s2);
         mtf::mtf_decode(s2, s1, orig_len);
